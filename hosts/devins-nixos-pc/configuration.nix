35196188e0a013b52b31a424c20a78fc19a3f7ba
intergrav <intergrav@proton.me>
2023-08-04 20:54:09 -0400

    Add pop shell extension

--- hosts/devins-nixos-pc/configuration.nix
+++ hosts/devins-nixos-pc/configuration.nix
@@ -141,6 +141,7 @@
     gnomeExtensions.clipboard-history
     gnomeExtensions.panel-corners
     gnomeExtensions.rounded-window-corners
+    gnomeExtensions.pop-shell
   ];
 
   # NixOS release version
